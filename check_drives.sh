deb11033f75978c43f19558619c22f58f17f8eba
Tim Pozar <github@lns.com>
2020-03-23 08:01:29 -0700

    Support FreeBSD's geom and make it more portable to find bash wherever it is.

--- check_drives.sh
+++ check_drives.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+# The following is for Linux' mdadm process...
 # Check the RAID1 volumes 
-result=`cat /proc/mdstat`
-if [[ "$result" =~ .*_U.* || "$result" =~ .*U_.* ]]
-then
-        echo "The MDADM RAID drive on $HOSTNAME may have problems";
-        cat /proc/mdstat;
+# result=`cat /proc/mdstat`
+# if [[ "$result" =~ .*_U.* || "$result" =~ .*U_.* ]]
+# then
+        # echo "The MDADM RAID drive on $HOSTNAME may have problems";
+        # cat /proc/mdstat;
+# fi 
+
+# The following is for FreeBSD's geom's process...
+result=`geom mirror status | grep mirror`
+if [[ $result != *"COMPLETE"* ]] ; then
+        echo "The geom RAID drive on $HOSTNAME may have problems";
+        geom mirror status;
 fi 
 
 # Check the ZFS pool...
